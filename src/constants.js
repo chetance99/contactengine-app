7492ac834153596a75b5b9a820f81ffa682911af
chetance99 <chetan.ce99@gmail.com>
2024-02-09 15:41:44 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const endpoints = {
+  getCountries: 'http://localhost:53897/api/SmsProviders/Countries',
+  postSmsProvider: 'http://localhost:53897/api/SmsProviders',
+  getSmsProvider: 'http://localhost:53897/api/SmsProviders'
+};
+
+export const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
